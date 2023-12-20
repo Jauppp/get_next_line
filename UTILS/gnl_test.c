c31ee6c631c817e984a5babccd9e4f7d61fbb51c
Jauppp <127675629+Jauppp@users.noreply.github.com>
2023-12-20 13:49:55 +0100

    Updated gnl_test.c

--- UTILS/gnl_test.c
+++ UTILS/gnl_test.c
@@ -10,25 +10,34 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 #include <fcntl.h>
 #include <stdio.h>
 
 int	main(void)
 {
 	int		fd1;
+	//int		fd2;
+	//int		i;
 	char	*linefd1;
+	char	*linefd2;
+
+	//i = 0;
 
 	printf("BUFFER = %d\n", BUFFER_SIZE);
-	fd1 = open("giantline_nl.txt", O_RDONLY);
+	fd1 = open("symphonie.txt", O_RDONLY);
+	// fd2 = open("the_hobbit.txt", O_RDONLY);
 	linefd1 = get_next_line(fd1);
+	// linefd2 = get_next_line(fd2);
 	while (linefd1)
 	{
 		printf("%s", linefd1);
 		free(linefd1);
 		linefd1 = get_next_line(fd1);
+		//printf("%s", linefd2);
+		//free(linefd2);
+		//linefd2 = get_next_line(fd2);
 	}
-	if (linefd1)
-		free(linefd1);
 	close(fd1);
+	//close(fd2);
 }
